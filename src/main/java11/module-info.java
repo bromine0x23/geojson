db89102d7c7f8ecec1fef233532eb2c7f956803a
Bromine0x23 <bromine0x23@163.com>
2020-09-20 21:20:13 +0800

    更新项目配置
    Signed-off-by: Bromine0x23 <bromine0x23@163.com>

---
+++ src/main/java11/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author <a href="mailto:bromine0x23@163.com">Bromine0x23</a>
+ */
+module cn.bromine0x23.geojson {
+	requires lombok;
+
+	exports cn.bromine0x23.geojson;
+	exports cn.bromine0x23.geojson.jackson;
+}
