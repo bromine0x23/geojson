95e02d84375f30dbb5df7830afa0b2ae3c96d690
Bromine0x23 <bromine0x23@163.com>
2020-10-28 23:28:10 +0800

    module-info.java
    Signed-off-by: Bromine0x23 <bromine0x23@163.com>

--- src/main/java11/module-info.java
+++ src/main/java11/module-info.java
@@ -2,7 +2,7 @@
  * @author <a href="mailto:bromine0x23@163.com">Bromine0x23</a>
  */
 module cn.bromine0x23.geojson {
-	requires lombok;
+	requires static lombok;
 
 	exports cn.bromine0x23.geojson;
 	exports cn.bromine0x23.geojson.jackson;
